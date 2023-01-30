e156f0ab02587c129b01fdf7b7dd9459b4a68de6
Dmitrii Ounap <ounap2016@yandex.ru>
2023-01-30 09:15:30 +0500

    IT WORKS!

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Character:
     RANGE_VALUE_DEFENCE = (1, 5)
     SPECIAL_BUFF = 15
     SPECIAL_SKILL = 'Удача'
-
+   
     def __init__(self, name):
         self.name = name
 
@@ -40,6 +40,7 @@ class Warrior(Character):
     RANGE_VALUE_DEFENCE = (5, 10)
     SPECIAL_BUFF = DEFAULT_STAMINA + 25
     SPECIAL_SKILL = 'Выносливость'
+    DEFAULT_ATTACK = 5
 
 
 class Mage(Character):
@@ -61,6 +62,7 @@ class Healer(Character):
 
 
 def start_training(character):
+
     print('Потренируйся управлять своими навыками.')
     print('Введи одну из команд: attack — чтобы атаковать противника, '
           'defence — чтобы блокировать атаку противника или '
@@ -69,12 +71,12 @@ def start_training(character):
     cmd = None
     while cmd != 'skip':
         cmd = input('Введи команду: ')
-        commands = {'attack': {character.attack},
-                    'defence': {character.defence},
-                    'special': {character.special}}
-        if cmd in commands.keys:
-            print(commands[cmd])
-            return 'Тренировка окончена'
+        commands = {'attack': character.attack,
+                    'defence': character.defence,
+                    'special': character.special}
+        if cmd in commands:
+            print(commands[cmd]())
+        return 'Тренировка окончена'
 
 
 def choice_char_class(char_name: str) -> Character:
@@ -103,10 +105,10 @@ if __name__ == '__main__':
     run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
-    char_name: str = input('...назови себя: ')
+    char_name: Character = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
-    char_class: str = choice_char_class()
-    print(start_training(char_name, char_class))
+    char_class: str = choice_char_class(char_name)
+    print(start_training(Character(char_name)))
